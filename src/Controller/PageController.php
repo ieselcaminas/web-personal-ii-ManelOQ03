740e6d3d7a912f0b78d8cf773621c7453158c10f
Manel <manortque@alu.edu.gva.es>
2025-12-09 11:19:11 +0000

    Para casa

---
+++ src/Controller/PageController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class PageController extends AbstractController
+{
+    #[Route('/', name: 'index')]
+    public function index(): Response
+    {
+        return $this->render('page/index.html.twig', []);
+    }
+
+    #[Route('/about', name: 'about')]
+    public function about(): Response
+    {
+        return $this->render('page/about.html.twig', []);
+    }
+
+    #[Route('/contact', name: 'contact')]
+    public function contact(): Response
+    {
+        return $this->render('page/contact.html.twig', []);
+    }
+
+    #[Route('/blog', name: 'blog')]
+    public function blog(): Response
+    {
+        return $this->render('page/blog.html.twig', []);
+    }
+
+    #[Route('/single_post', name: 'single_post')]
+    public function single_post(): Response
+    {
+        return $this->render('page/single_post.html.twig', []);
+    }
+}
